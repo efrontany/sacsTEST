35e8dc1ae1afa4bb1fceb7a248fb2cd69fa82efc
Elson J. Frontany <elson.frontany@apex.pr>
2012-10-25 11:07:57 -0400

    Implemented printThis and printThat

--- booh.cs
+++ booh.cs
@@ -1,3 +1,13 @@
 void main(void){
 	// This is another file
+	// with printThis and printThat implementation
+}
+
+
+string printThis(void){
+	printf("This");
+}
+
+string printThat(void){
+	printf("That");
 }
